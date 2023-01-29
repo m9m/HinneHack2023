09f49c01cac7a7f1549317a3f78153bad63e790c
m9m <95250150+m9m@users.noreply.github.com>
2023-01-29 12:35:51 -0600

    Finalized bug fixes

--- app.py
+++ app.py
@@ -1,41 +1,22 @@
-from flask import Flask, redirect, url_for, request, abort, jsonify, render_template, send_from_directory, logging, session, g, flash
-import flask, logging, functools, sqlite3
-from flask_limiter import Limiter
-from datetime import timedelta
-from flask_login import LoginManager, current_user, login_user, login_required, UserMixin, logout_user, AnonymousUserMixin
-from flask_limiter.util import get_remote_address
+from flask import Flask, redirect, url_for, request, abort, render_template, session
+import os, json, requests, functools
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_wtf.recaptcha import RecaptchaField
-from itsdangerous import URLSafeTimedSerializer
-from flask_mail import Message, Mail
-import threading
-from datetime import datetime, timedelta
-from authlib.integrations.flask_client import OAuth
-import os, json, requests
-import hashlib
 from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
 from pip._vendor import cachecontrol
 import google.auth.transport.requests
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired
-from wtforms import StringField, SubmitField, SelectField
+from wtforms import StringField, SubmitField, SelectField, TextAreaField, HiddenField, BooleanField
 from  sqlalchemy.ext.mutable import MutableList
-
-#blueprints
-from blueprints.index import route as ir
-
 
 with open("auth/keys.json", "r") as fd:
     insecure_data = json.load(fd)
 app = Flask(__name__, static_folder="static")
-app.register_blueprint(ir)
 
-#basic settings
+
+# env settings
 app.testing = True
-app.debug=True
-superuser = ['']
+app.debug = True
 
 
 # define login constants
@@ -58,23 +39,48 @@ app.config['RECAPTCHA_PRIVATE_KEY'] = ""
 app.config['RECAPTCHA_DATA_ATTRS'] = {'theme': 'light'}
 
 
-#define forms
+# define forms
 class SearchCommunityForm(FlaskForm):
     city_name = StringField()
     submit = SubmitField()
 
 class ApplyCommunityForm(FlaskForm):
     city_name = StringField()
+    confirm = BooleanField("Confirm")
+    submit = SubmitField()
+
+class VeiwCityForm(FlaskForm):
+    form_n = HiddenField("2")
+    city_name = StringField()
+    confirm = BooleanField("Confirm")
+    submit = SubmitField()
+
+class ViewOficialForm(FlaskForm):
+    form_n = HiddenField("3")
+    city_name = HiddenField()
+    title = StringField()
+    name = StringField()
+    contact = StringField()
+    confirm = BooleanField("Confirm")
     submit = SubmitField()
 
 class CreatePetitionForm(FlaskForm):
+    form_n = HiddenField("0")
     city_name = SelectField()
     petition_name = StringField()
-    petition_description = StringField()
+    petition_description = TextAreaField()
+    submit = SubmitField()
+
+class AddOfficialInfoForm(FlaskForm):
+    form_n = HiddenField("1")
+    city_name = SelectField()
+    title = StringField()
+    name = StringField()
+    contact = StringField()
     submit = SubmitField()
 
 
-#Define database
+# define database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy()
@@ -87,15 +93,47 @@ def create_table():
         db.session.add(LiveCities("Minneapolis", True))
     db.session.commit()
 
+# define database tables
 class LiveCities(db.Model):
     __tablename__ = 'livecities'
     city_name = db.Column(db.String(30), primary_key=True, unique=True)
     live = db.Column(db.Boolean(), default=False, nullable=False)
+    officials = db.Column(db.String(10000))
 
-    def __init__(self, city_name, live=True):
+    def __init__(self, city_name, live=False, moderator=False, officials=json.dumps({})):
         self.city_name = city_name
         self.live = live
+        self.moderator = moderator
+        self.officials = officials
     
+    def add_official(self, title, name, contact):
+        data = json.loads(self.officials)
+        if name in data:
+            return False
+        data[name] = {"name":name, "title":title, "contact":contact, "verified": False}
+        self.officials = json.dumps(data)
+        return True
+
+    def remove_official(self, name):
+        data = json.loads(self.officials)
+        if name in data:
+            del data[name]
+            self.officials = json.dumps(data)
+            return True
+        else:
+            return False
+        
+    def any_unverified(self):
+        data = json.loads(self.officials)
+        for user in data:
+            if not data[user]["verified"]:
+                return data[user]["title"], data[user]["name"], data[user]["contact"]
+
+    def verify_official(self, name):
+        data = json.loads(self.officials)
+        data[name]["verified"] = True
+        self.officials = json.dumps(data)
+
     def push_live(self):
         self.live = True
 
@@ -144,11 +182,12 @@ def index():
 
 @app.route('/home', methods=['GET'])
 def home(): 
-    return redirect(url_for("login"))
+    return render_template("home.html")
 
 def check_oauth(function):  #a function to check if the user is authorized or not
     @functools.wraps(function)
     def wrapper(*args, **kwargs):
+        print(session.get("google_id"))
         if not session.get("google_id"):
             if request.method == "POST":
                 return {"Error":"Unuathenticated"}, 403
@@ -168,6 +207,8 @@ def login():
 def logout():
     session.clear() #wipe server side session data
     return redirect(url_for("index"))
+
+# authentication integration routes
 
 @app.route('/auth/google')
 def auth_googles():
@@ -192,11 +233,11 @@ def callback():
         request=token_request,
         audience=GOOGLE_CLIENT_ID
     )
-    print(id_info)
-    session["google_id"] = id_info.get("sub")
+    
+    session["google_id"] = id_info.get("email")
     session["name"] = id_info.get("name")
     session["profile_pic"] = id_info.get("picture")
-    session["authenticated"] = True
+    session["admin"] = any(session.get('google_id') == email for email in insecure_data.get('admin_google_ids'))
     return redirect("/home")
 
 def create_id():
@@ -205,23 +246,72 @@ def create_id():
     else:
         return 0
 
+# management routes
+
 @app.route('/dashboard', methods=["GET","POST"])
 @check_oauth
 def dashboard():
     form = CreatePetitionForm()
+    form1 = AddOfficialInfoForm()
     form.city_name.choices = [(q.city_name, q.city_name) for q in LiveCities.query.order_by('city_name')]
+    form1.city_name.choices = [(q.city_name, q.city_name) for q in LiveCities.query.order_by('city_name')]
+    form2 = VeiwCityForm()
+    form3 = ViewOficialForm()
     if request.method == "GET":
-        return render_template("dashboard.html", form=form, message="", petition=Petition)
-    poster_id = session['google_id']
-    city_name = request.form['city_name']
-    petition_name = request.form["petition_name"]
-    petition_description = request.form["petition_description"]
-    if Petition.query.filter_by(poster_id=poster_id).first():
-        return render_template("dashboard.html", form=form, message="You already have a petition active.", petition=Petition)
-    record = Petition(create_id(), poster_id, city_name, petition_name, petition_description)
-    db.session.add(record)
-    db.session.commit()
-    return render_template("dashboard.html", form=form, message="Your petition is live! View it on the "+city_name+" page!", petition=Petition)
+        return render_template("dashboard.html", form=form, form1=form1, form2=form2, message="", message1="", petition=Petition, form3=form3, livecities=LiveCities)
+    form_n = request.form['form_n']
+    if form_n == "0": # add petition
+        poster_id = session['google_id']
+        city_name = request.form['city_name']
+        petition_name = request.form["petition_name"]
+        petition_description = request.form["petition_description"]
+        if Petition.query.filter_by(poster_id=poster_id).first():
+            return render_template("dashboard.html", form=form, form1=form1, form2=form2, message="You already have a petition active.", livecities=LiveCities, petition=Petition)
+        record = Petition(create_id(), poster_id, city_name, petition_name, petition_description)
+        db.session.add(record)
+        db.session.commit()
+        return render_template("dashboard.html", form=form, form2=form2,message="Your petition is live! View it on the "+city_name+" page!", livecities=LiveCities, form1=form1, petition=Petition)
+    elif form_n == "1": # add city official
+        city_name = request.form['city_name']
+        name = request.form["name"]
+        title = request.form["title"]
+        contact = request.form["contact"]
+        city = LiveCities.query.filter_by(city_name=city_name).first()
+        if city.add_official(title, name, contact):
+            db.session.commit()
+            return render_template("dashboard.html", form2=form2, form3=form3, livecities=LiveCities, form=form, message1="Added the official to the city!", form1=form1, petition=Petition)
+        else:
+            return render_template("dashboard.html", form2=form2, form3=form3, livecities=LiveCities, form=form, message1="Oficial already added.", form1=form1, petition=Petition)
+    elif form_n == "2": # moderate cities requested
+        city_name = request.form['city_name']
+        if not request.form.get('confirm'):
+            city = LiveCities.query.filter_by(city_name=city_name).first()
+            db.session.delete(city)
+            db.session.commit()
+            return render_template("dashboard.html", form2=form2, form3=form3, livecities=LiveCities, form=form, message2=city.city_name+" was removed from que!", form1=form1, petition=Petition)
+
+        else:
+            city = LiveCities.query.filter_by(city_name=city_name).first()
+            city.push_live()
+            db.session.commit()
+            return render_template("dashboard.html", form2=form2, form3=form3, livecities=LiveCities, form=form, message2=city.city_name+" was pushed live!", form1=form1, petition=Petition)
+    elif form_n == "3": # moderate officials requested
+        city_name = request.form['city_name']
+        if not request.form.get('confirm'):
+            city = LiveCities.query.filter_by(city_name=city_name).first()
+            name=city.any_unverified()[1]
+            city.remove_official(name)
+            db.session.commit()
+            return render_template("dashboard.html", form2=form2, form3=form3, livecities=LiveCities, form=form, message3=name+" was removed from que!", form1=form1, petition=Petition)
+        else:
+            
+            city = LiveCities.query.filter_by(city_name=city_name).first()
+            name = city.any_unverified()[1]
+            city.verify_official(name)
+            db.session.commit()
+            return render_template("dashboard.html", form2=form2, form3=form3, livecities=LiveCities, form=form, message3=name+" was verified!", form1=form1, petition=Petition)
+
+# community routes
 
 @app.route('/communities/')
 def communities():
@@ -235,12 +325,13 @@ def communities_search():
     name = request.form["city_name"]
     searched_city = LiveCities.query.filter_by(city_name=name).first()
     if not searched_city:
-        return render_template("search.html", form=form, message="City not found. Please make sure the name is correct. Otherwise <a href=\"/communities/apply\">Apply for a city to be added</a>")
+        return render_template("search.html", form=form, message="City not found. Please make sure the name is correct, otherwise Add the City.")
     if not searched_city.get_live():
         return render_template("application.html", form=form, message="This city has been suggested and is being reviewed, hang tight!")
     return redirect(url_for("communities_view", name=name))
 
 @app.route('/communities/apply',methods=["GET","POST"])
+@check_oauth
 def communities_apply():
     form = ApplyCommunityForm()
     if request.method == "GET":
@@ -255,7 +346,7 @@ def communities_apply():
     record = LiveCities(city_name=name)
     db.session.add(record)
     db.session.commit()
-    return render_template("application.html", form=form, message="Your application has b een submitted to a site moderator and will be reviewed within the next 24h, hang tight while we create your community page!")
+    return render_template("application.html", form=form, message="Your application has been submitted to a site moderator and will be reviewed within the next 24h, hang tight while we create your community page!")
 
 @app.route('/communities/<name>',methods=["GET","POST"])
 def communities_view(name):
@@ -264,7 +355,9 @@ def communities_view(name):
         return redirect(url_for("communities_search"))
     if not city.get_live():
         return redirect(url_for("communities_search"))
-    return render_template("communities.html", community=city, petition=Petition)
+    return render_template("communities.html", community=city, petition=Petition, officials=json.loads(city.officials))
+
+# petition routes
 
 @app.route('/petitions/<id>',methods=["GET","POST"])
 def petitions(id):
@@ -278,15 +371,27 @@ def petitions(id):
 @app.route('/petitions/<id>/sign',methods=["POST"])
 def sign(id):
     petition = Petition.query.filter_by(id=id).first() #exists
-    if not petition:
+    if not petition or not session.get('google_id'):
         return {"success":"false","message":"Petition id not found"}, 400
-    if petition.sign(session['google_id']):
+    if petition.sign(session.get('google_id')):
         db.session.commit()
         return {"success":"true","message":"Petition signed!"}, 200
     else:
         return {"success":"false","message":"Petition already signed."}, 400
-        
 
+
+@check_oauth
+@app.route('/petitions/<id>/delete',methods=["POST"])
+def delete(id):
+    petition = Petition.query.filter_by(id=id).first() #exists
+    if not petition or not petition.poster_id==session["google_id"]:
+        return {"success":"false","message":"Petition cannot be deleted"}, 400
+    db.session.delete(petition)
+    db.session.commit()
+    return {"success":"true","message":"Petition deleted!"}, 200
+
+        
+#error handlers
 
 @app.errorhandler(429)
 def rate_limit(e):
@@ -302,7 +407,7 @@ def page_not_found(e):
     if request.method == 'POST':
         return {"status":"fail","errors":["the requeted resource wasn\'t found"]}, 403
     else:
-        return ""
+        return render_template("404.html")
 
 @app.errorhandler(403)
 def page_not_found(e):
@@ -311,10 +416,7 @@ def page_not_found(e):
     else:
         return '<html>\n<h1>403 Forbidden</h1>\n<br>\n<br>\n<p>You have been denied from viewing the resource</html>'
 
-
-
-
-
+# serve the app!
 if __name__ == '__main__':
     #from waitress import serve
     #serve(app, host=hst, port=prt, url_scheme='https')
